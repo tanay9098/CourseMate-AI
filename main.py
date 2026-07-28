1e9a1cc0ba5927f15c7288d6129f7a06f71dd12b
Tanay Dwivedi <tanaydwivedi678@gmail.com>
2026-07-28 14:23:23 +0530

    feat:added document loaders text and other stuffs

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
+
+data = TextLoader("documentLoaders/Striver Sheet.xlsx")
+docs= data.load()
+
+template=ChatPromptTemplate.from_messages(
+    [("system", "you are an AI that summarizes the text "),
+     ("human", "{data}")
+
+    ]
+)
+
+model=ChatMistralAI(model= "mistral-small-2506")
+
+result=model.invoke("hello")
+
+print(result.content)
+
+
