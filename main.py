384edb1391143057723ad5f63e25a34876e23445
tanay9098 <tanaydwivedi678@gmail.com>
2026-08-29 15:56:15 +0530

    feat:added feature to read and summarise the docs from pdf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
-from langchain_community.document_loaders import UnstructuredExcelLoader
+from langchain_community.document_loaders import UnstructuredExcelLoader, PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
@@ -8,7 +8,17 @@ load_dotenv()
 data = UnstructuredExcelLoader("documentLoaders/Striver Sheet.xlsx", mode="elements")
 docs= data.load()
 
+data2 = PyPDFLoader("documentLoaders/DSA documents/3. Sorting.pdf")
+docs2 = data2.load()
+
 template=ChatPromptTemplate.from_messages(
+    [("system", "you are an AI that summarizes the text "),
+     ("human", "{data}")
+
+    ]
+)
+
+template2=ChatPromptTemplate.from_messages(
     [("system", "you are an AI that summarizes the text "),
      ("human", "{data}")
 
@@ -21,6 +31,14 @@ prompt=template.format_prompt(data=docs[0].page_content)
 
 result=model.invoke(prompt)
 
-print(result.content)
+model2=ChatMistralAI(model= "mistral-small-2506")
 
+prompt2=template2.format_prompt(data=docs2[0].page_content)
+
+result2=model2.invoke(prompt2)
+
+
+
+print(result.content)
+print(result2.content)
 
